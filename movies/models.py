ca3f2c5aadcd9fcba88223b7b903b8262715814a
Germano Nascimento <nascimento.germano@gmail.com>
2025-01-16 06:06:22 -0300

    adicianada a app movies, views, urls e serializers

--- movies/models.py
+++ movies/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
     release_date = models.DateField(null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name='movie')
-    resume = models.TimeField(null=True, blank=True)
+    resume = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
