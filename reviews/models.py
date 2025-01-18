c71e59892c32d042ff37cc04e27fabd3d47bfd43
Germano Nascimento <nascimento.germano@gmail.com>
2025-01-18 15:14:38 -0300

    adicionada a app review e seu models

---
+++ reviews/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from movies.models import Movie
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='reviews')
+    stars = models.IntegerField(
+        validators=[MinValueValidator(0, 'Nota não pode ser inferior a Zero.'),
+                    MaxValueValidator(5, 'Nota não pode ser superior a Cinco.')]
+        )
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.movie
