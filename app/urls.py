aa97b38e209f5757c9986aaa62a3b65d0c887997
Germano Nascimento <nascimento.germano@gmail.com>
2025-01-18 16:19:24 -0300

    serializers da pasta review adicionada, view modificada para receber serializers, url do app com novos paths

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 from movies.views import MovieCreateListView, MovieRetriveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetriveUpdateDestroyView
 
 
 urlpatterns = [
@@ -16,4 +17,7 @@ urlpatterns = [
 
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>/', MovieRetriveUpdateDestroyView.as_view(), name='movie-detail-view'),
+
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
+    path('reviews/<int:pk>/', ReviewRetriveUpdateDestroyView.as_view(), name='review-detail-view'),
 ]
