39d6664cc0a8858d8e7824b4ac45d427f21e6240
Germano Nascimento <nascimento.germano@gmail.com>
2025-02-01 16:05:06 -0300

    Adicionado o ValidationError ao serializer.py do movies

--- movies/serializer.py
+++ movies/serializer.py
@@ -6,3 +6,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1980:
+            raise serializers.ValidationError("Favor não lançar filmes antes de 1980")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("Resumo não suporta mais de 200 caracteres.")
+        return value
