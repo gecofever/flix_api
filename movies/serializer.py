24e3bb04fedafb98de2e05ef29d8d9786320dc5d
Germano Nascimento <nascimento.germano@gmail.com>
2025-02-06 06:07:02 -0300

    preparando para colocar o rate dos filmes

--- movies/serializer.py
+++ movies/serializer.py
@@ -3,16 +3,22 @@ from movies.models import Movie
 
 class MovieModelSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = "__all__"
 
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+        return 5
+
     def validate_release_date(self, value):
-        if value.year < 1980:
+        if value.year < 1950:
             raise serializers.ValidationError("Favor não lançar filmes antes de 1980")
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 300:
             raise serializers.ValidationError("Resumo não suporta mais de 200 caracteres.")
         return value
