8daad35608e121adb6642bae31417c1d822b6bf4
Germano Nascimento <nascimento.germano@gmail.com>
2025-03-27 20:30:15 -0300

    Alterada a permissão para Global Permission

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('movies/', views.MovieCreateListView.as_view(), name='genre-create-list'),
     path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    path('movies/stats/', views.MovieStatsView.as_view(), name='movies-stast-view'),
 ]
